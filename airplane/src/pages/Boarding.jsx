1b9c64320dd6fe8f874afac2ebf511a24dbb8cb6
john48k⌖ <126651207+john48k@users.noreply.github.com>
2024-11-15 17:17:05 +0800

    Flight Page Done

--- airplane/src/pages/Boarding.jsx
+++ airplane/src/pages/Boarding.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { Navbar } from "../components/Navbar";
 import { Link } from "react-router-dom";
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Button from "@mui/material/Button";
 
 // import Carousel from "react-bootstrap/Carousel";
 // import Accordion from "@mui/material/Accordion";
@@ -57,105 +64,104 @@ export const Boarding = () => {
 
   return (
     <>
-      <div>
-        <MDBFooter
-          bgColor="light"
-          className="text-center text-lg-start text-muted"
-        >
-          <section className="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
-            <div
-              className="me-5 d-none d-lg-block"
-              style={{ fontSize: "0.875rem" }}
-            >
-              <p className="mb-0">
-                <MDBIcon icon="envelope" className="me-2" />
-                CitUniversity@gmail.com |
-                <MDBIcon icon="map-marker-alt" className="me-2 ms-2" />
-                7VVJ+QFR, Natalio B. Bacalso Ave, Cebu City, 6000 Cebu
-              </p>
-            </div>
+      <MDBFooter
+        bgColor="light"
+        className="text-center text-lg-start text-muted"
+      >
+        <section className="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
+          <div
+            className="me-5 d-none d-lg-block"
+            style={{ fontSize: "0.875rem" }}
+          >
+            <p className="mb-0">
+              <MDBIcon icon="envelope" className="me-2" />
+              CitUniversity@gmail.com |
+              <MDBIcon icon="map-marker-alt" className="me-2 ms-2" />
+              7VVJ+QFR, Natalio B. Bacalso Ave, Cebu City, 6000 Cebu
+            </p>
+          </div>
 
-            <div>
-              <a href="https://www.facebook.com/" className="me-3 text-reset  ">
-                <MDBIcon fab icon="facebook-f" />
-              </a>
-              <a href="https://twitter.com/" className="me-3 text-reset">
-                <MDBIcon fab icon="twitter" />
-              </a>
-              <a href="https://www.google.com/" className="me-3 text-reset">
-                <MDBIcon fab icon="google" />
-              </a>
-              <a href="https://www.instagram.com/" className="me-3 text-reset">
-                <MDBIcon fab icon="instagram" />
-              </a>
-              <a href="https://www.linkedin.com/" className="me-3 text-reset">
-                <MDBIcon fab icon="linkedin" />
-              </a>
-              <a href="https://www.github.com/" className="me-3 text-reset">
-                <MDBIcon fab icon="github" />
-              </a>
-            </div>
-          </section>
-        </MDBFooter>
-        <Navbar activeTab="Boarding" />
-        <div className="boarding-banner-page-heading">
-          <div className="banner-heading-text">BOARDING</div>
-        </div>
+          <div>
+            <a href="https://www.facebook.com/" className="me-3 text-reset  ">
+              <MDBIcon fab icon="facebook-f" />
+            </a>
+            <a href="https://twitter.com/" className="me-3 text-reset">
+              <MDBIcon fab icon="twitter" />
+            </a>
+            <a href="https://www.google.com/" className="me-3 text-reset">
+              <MDBIcon fab icon="google" />
+            </a>
+            <a href="https://www.instagram.com/" className="me-3 text-reset">
+              <MDBIcon fab icon="instagram" />
+            </a>
+            <a href="https://www.linkedin.com/" className="me-3 text-reset">
+              <MDBIcon fab icon="linkedin" />
+            </a>
+            <a href="https://www.github.com/" className="me-3 text-reset">
+              <MDBIcon fab icon="github" />
+            </a>
+          </div>
+        </section>
+      </MDBFooter>
+      <Navbar activeTab="Boarding" />
+      <div className="boarding-banner-page-heading">
+        <div className="banner-heading-text">BOARDING</div>
+      </div>
 
-        <MDBContainer fluid className="p-4">
-          <MDBRow>
-            <MDBCol
-              md="6"
-              className="text-center text-md-start d-flex flex-column justify-content-center"
-            >
-              <h1 className="my-5 display-3 fw-bold ls-tight px-3">
-                Fasten Your Seatbelts <br />
-                <span className="text-primary">Your Journey Begins Here!</span>
-              </h1>
+      <MDBContainer fluid className="p-4">
+        <MDBRow>
+          <MDBCol
+            md="6"
+            className="text-center text-md-start d-flex flex-column justify-content-center"
+          >
+            <h1 className="my-5 display-3 fw-bold ls-tight px-3">
+              Fasten Your Seatbelts <br />
+              <span className="text-primary">Your Journey Begins Here!</span>
+            </h1>
 
-              <p className="px-3" style={{ color: "hsl(217, 10%, 50.8%)" }}>
-                Boarding isn’t just a step onto the plane; it’s the first
-                chapter of your journey. With every step, you’re moving closer
-                to a world of possibilities, whether it’s a new destination, a
-                family reunion, or an important business meeting. We strive to
-                make boarding a seamless experience, designed to ease you from
-                the terminal to take-off with comfort and efficiency. Each
-                moment counts, and we’re here to ensure that your transition is
-                smooth, your seat is ready, and your adventure awaits.
-              </p>
-            </MDBCol>
+            <p className="px-3" style={{ color: "hsl(217, 10%, 50.8%)" }}>
+              Boarding isn’t just a step onto the plane; it’s the first chapter
+              of your journey. With every step, you’re moving closer to a world
+              of possibilities, whether it’s a new destination, a family
+              reunion, or an important business meeting. We strive to make
+              boarding a seamless experience, designed to ease you from the
+              terminal to take-off with comfort and efficiency. Each moment
+              counts, and we’re here to ensure that your transition is smooth,
+              your seat is ready, and your adventure awaits.
+            </p>
+          </MDBCol>
 
-            <MDBCol md="6">
-              <MDBCard className="my-5">
-                <MDBCardBody className="p-5">
-                  <h1>BOARDING PASS</h1>
+          <MDBCol md="6">
+            <MDBCard className="my-5">
+              <MDBCardBody className="p-5">
+                <h1>BOARDING PASS</h1>
 
-                  <form onSubmit={handleSubmit}>
-                    <MDBInput
-                      wrapperClass="mb-4"
-                      label="Seat Number"
-                      id="seatNumber"
-                      type="text"
-                      value={seatNumber}
-                      required
-                      onChange={(event) => setSeatNumber(event.target.value)}
-                    />
+                <form onSubmit={handleSubmit}>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Seat Number"
+                    id="seatNumber"
+                    type="text"
+                    value={seatNumber}
+                    required
+                    onChange={(event) => setSeatNumber(event.target.value)}
+                  />
 
-                    <MDBInput
-                      wrapperClass="mb-4"
-                      label="Boarding Time"
-                      id="boardingTime"
-                      type="datetime-local"
-                      value={boardingTime}
-                      onChange={(event) => setBoardingTime(event.target.value)}
-                    />
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Boarding Time"
+                    id="boardingTime"
+                    type="datetime-local"
+                    value={boardingTime}
+                    onChange={(event) => setBoardingTime(event.target.value)}
+                  />
 
-                    <MDBBtn type="submit" className="w-100 mb-4" size="md">
-                      Submit
-                    </MDBBtn>
-                  </form>
+                  <MDBBtn type="submit" className="w-100 mb-4" size="md">
+                    Submit
+                  </MDBBtn>
+                </form>
 
-                  {/* <div className="d-flex justify-content-center mb-4">
+                {/* <div className="d-flex justify-content-center mb-4">
                     <MDBCheckbox
                       name="flexCheck"
                       value=""
@@ -164,109 +170,138 @@ export const Boarding = () => {
                     />
                   </div> */}
 
-                  {/* <MDBBtn className="w-100 mb-4" size="md">
+                {/* <MDBBtn className="w-100 mb-4" size="md">
                     sign up
                   </MDBBtn> */}
 
-                  <div className="text-center">
-                    <p>Check Out Our Other Pages:</p>
+                <div className="text-center">
+                  <p>Check Out Our Other Pages:</p>
 
-                    <MDBBtn
-                      tag="a"
-                      href="https://facebook.com"
-                      target="_blank"
-                      color="none"
-                      className="mx-3"
-                      style={{ color: "#1266f1" }}
-                    >
-                      <MDBIcon fab icon="facebook-f" size="sm" />
-                    </MDBBtn>
+                  <MDBBtn
+                    tag="a"
+                    href="https://facebook.com"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="facebook-f" size="sm" />
+                  </MDBBtn>
 
-                    <MDBBtn
-                      tag="a"
-                      href="https://twitter.com"
-                      target="_blank"
-                      color="none"
-                      className="mx-3"
-                      style={{ color: "#1266f1" }}
-                    >
-                      <MDBIcon fab icon="twitter" size="sm" />
-                    </MDBBtn>
+                  <MDBBtn
+                    tag="a"
+                    href="https://twitter.com"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="twitter" size="sm" />
+                  </MDBBtn>
 
-                    <MDBBtn
-                      tag="a"
-                      href="https://google.com"
-                      target="_blank"
-                      color="none"
-                      className="mx-3"
-                      style={{ color: "#1266f1" }}
-                    >
-                      <MDBIcon fab icon="google" size="sm" />
-                    </MDBBtn>
+                  <MDBBtn
+                    tag="a"
+                    href="https://google.com"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="google" size="sm" />
+                  </MDBBtn>
 
-                    <MDBBtn
-                      tag="a"
-                      href="https://github.com/john48k/airplane-boarding-system"
-                      target="_blank"
-                      color="none"
-                      className="mx-3"
-                      style={{ color: "#1266f1" }}
-                    >
-                      <MDBIcon fab icon="github" size="sm" />
-                    </MDBBtn>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
+                  <MDBBtn
+                    tag="a"
+                    href="https://github.com/john48k/airplane-boarding-system"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="github" size="sm" />
+                  </MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
 
-        {/* <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="seatNumber">Seat Number:</label>
-            <input
-              type="text"
-              id="seatNumber"
-              value={seatNumber}
-              onChange={(event) => setSeatNumber(event.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="boardingTime">Boarding Time:</label>
-            <input
-              type="text"
-              id="boardingTime"
-              value={boardingTime}
-              onChange={(event) => setBoardingTime(event.target.value)}
-            />
-          </div>
-          <button type="submit">Submit</button>
-        </form> */}
-      </div>
-      <MDBFooter
-        className="text-center text-white"
-        style={{ backgroundColor: "#555C67" }}
-      >
-        <MDBContainer className="p-4 pb-0">
-          <section className="">
-            <p className="d-flex justify-content-center align-items-center">
-              <span className="me-3">Register for free</span>
-              <Link to="/signup">
-                <MDBBtn type="button" outline color="light" rounded>
-                  Sign up!
-                </MDBBtn>
-              </Link>
-            </p>
-          </section>
-        </MDBContainer>
-
-        <div
-          className="text-center p-3"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-        >
-          © 2024 FLIGHT MATCH. All Rights Reserved
+      {/* <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="seatNumber">Seat Number:</label>
+          <input
+            type="text"
+            id="seatNumber"
+            value={seatNumber}
+            onChange={(event) => setSeatNumber(event.target.value)}
+          />
         </div>
-      </MDBFooter>
+        <div>
+          <label htmlFor="boardingTime">Boarding Time:</label>
+          <input
+            type="text"
+            id="boardingTime"
+            value={boardingTime}
+            onChange={(event) => setBoardingTime(event.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form> */}
+
+      {/* <div className="board-pics-container">
+        <MDBCarousel interval={1000} className="boarding-custom-carousel">
+          <MDBCarouselItem itemId={1}>
+            <img
+              src="/images/board-4.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId={2}>
+            <img
+              src="/images/board-2.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId={3}>
+            <img
+              src="/images/board-3.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId={4}>
+            <img
+              src="/images/board-6.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId={5}>
+            <img
+              src="/images/board-5.webp"
+              className="d-block w-100"
+              alt="..."
+            />
+          </MDBCarouselItem>
+        </MDBCarousel>
+      </div>
+*/}
+      <div>
+        <MDBFooter
+          className="text-center text-white"
+          style={{ backgroundColor: "#555C67" }}
+        >
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2024 FLIGHT MATCH. All Rights Reserved
+          </div>
+        </MDBFooter>
+      </div>
     </>
   );
 };
--- airplane/src/pages/Flight.jsx
+++ airplane/src/pages/Flight.jsx
@@ -18,7 +18,11 @@ import {
   MDBRow,
   MDBBtn,
   MDBIcon,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
 } from "mdb-react-ui-kit";
+import { MDBSelect } from "mdb-react-ui-kit";
 
 export const Flight = () => {
   const [formData, setFormData] = useState({
@@ -104,6 +108,187 @@ export const Flight = () => {
       <div className="flight-banner-page-heading">
         <div className="banner-heading-text">FLIGHTS</div>
       </div>
+      <MDBContainer fluid className="p-4">
+        <MDBRow>
+          <MDBCol
+            md="6"
+            className="text-center text-md-start d-flex flex-column justify-content-center"
+          >
+            <h1 className="my-5 display-3 fw-bold ls-tight px-3">
+              Fasten Your Seatbelts <br />
+              <span className="text-primary">Your Journey Begins Here!</span>
+            </h1>
+
+            <p className="px-3" style={{ color: "hsl(217, 10%, 50.8%)" }}>
+              Boarding isn’t just a step onto the plane; it’s the first chapter
+              of your journey. With every step, you’re moving closer to a world
+              of possibilities, whether it’s a new destination, a family
+              reunion, or an important business meeting. We strive to make
+              boarding a seamless experience, designed to ease you from the
+              terminal to take-off with comfort and efficiency. Each moment
+              counts, and we’re here to ensure that your transition is smooth,
+              your seat is ready, and your adventure awaits.
+            </p>
+          </MDBCol>
+
+          <MDBCol md="6">
+            <MDBCard className="my-5">
+              <MDBCardBody className="p-5">
+                <h1>FLIGHTS DETAILS</h1>
+
+                <form onSubmit={handleSubmit}>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Flight Number"
+                    id="flightNumber"
+                    type="number"
+                    name="flight_number"
+                    value={formData.flight_number}
+                    onChange={handleChange}
+                    required
+                    // min="1"
+                    // step="1"
+                  />
+
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Departure Time"
+                    id="departureTime"
+                    type="datetime-local"
+                    name="departure_time"
+                    value={formData.departure_time}
+                    onChange={handleChange}
+                    required
+                  />
+
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Arrival Time"
+                    id="arrivalTime"
+                    type="datetime-local"
+                    name="arrival_time"
+                    value={formData.arrival_time}
+                    onChange={handleChange}
+                    required
+                  />
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Flight Status"
+                    id="flightStatus"
+                    type="select"
+                    name="flight_status"
+                    value={formData.flight_status}
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="" disabled>
+                      Choose here
+                    </option>
+                    <option value="0">BOARDING</option>
+                    <option value="1">ON_FLIGHT</option>
+                    <option value="2">ARRIVED</option>
+                  </MDBInput>
+
+                  <div className="delete-update-button-container ">
+                    <MDBBtn
+                      type="button"
+                      className="update-button flight-update-color"
+                      size="md"
+                      onClick={() => navigate("/Flightupdate")}
+                    >
+                      Update Flight
+                    </MDBBtn>
+
+                    <MDBBtn
+                      type="button"
+                      className="delete-button flight-delete-color"
+                      size="md"
+                      onClick={() => navigate("/Flightdelete")}
+                    >
+                      Delete a Flight
+                    </MDBBtn>
+                  </div>
+
+                  <MDBBtn
+                    type="submit"
+                    className="w-100 mb-4 flight-create-color"
+                    size="md"
+                  >
+                    Create Flight
+                  </MDBBtn>
+                  {/* 
+                  <MDBBtn type="submit" className="w-100 mb-4" size="md">
+                    Submit
+                  </MDBBtn> */}
+                </form>
+
+                {/* <div className="d-flex justify-content-center mb-4">
+                    <MDBCheckbox
+                      name="flexCheck"
+                      value=""
+                      id="flexCheckDefault"
+                      label="Subscribe to our newsletter"
+                    />
+                  </div> */}
+
+                {/* <MDBBtn className="w-100 mb-4" size="md">
+                    sign up
+                  </MDBBtn> */}
+
+                <div className="text-center">
+                  <p>Check Out Our Other Pages:</p>
+
+                  <MDBBtn
+                    tag="a"
+                    href="https://facebook.com"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="facebook-f" size="sm" />
+                  </MDBBtn>
+
+                  <MDBBtn
+                    tag="a"
+                    href="https://twitter.com"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="twitter" size="sm" />
+                  </MDBBtn>
+
+                  <MDBBtn
+                    tag="a"
+                    href="https://google.com"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="google" size="sm" />
+                  </MDBBtn>
+
+                  <MDBBtn
+                    tag="a"
+                    href="https://github.com/john48k/airplane-boarding-system"
+                    target="_blank"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="github" size="sm" />
+                  </MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+      {/*       
       <div className="container">
         <h1>Flight Management</h1>
         <form id="flightForm" onSubmit={handleSubmit}>
@@ -172,6 +357,19 @@ export const Flight = () => {
             Delete a Flight
           </button>
         </div>
+      </div> */}
+      <div>
+        <MDBFooter
+          className="text-center text-white"
+          style={{ backgroundColor: "#555C67" }}
+        >
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2024 FLIGHT MATCH. All Rights Reserved
+          </div>
+        </MDBFooter>
       </div>
     </div>
   );
